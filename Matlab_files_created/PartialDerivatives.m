6739be1010be1d7bc761396010b94c2659ffe2e3
Daniel Herink <engineertandcode@gmail.com>
2016-11-22 22:52:23 -0800

    Created function to compute partial derivatives of the two images
    Modified main to include this new function:
    -Removed old code which used Gaussian kernels
    
    Deleted Gaussian kernel files:
    -No longer being used

---
+++ Matlab_files_created/PartialDerivatives.m
@@ -0,0 +1,10 @@
+function [Ix, Iy, It] = PartialDerivatives(Image1, Image2)
+    kernel_x = 1/4*[-1 1; -1 1];
+    kernel_y = 1/4*[1 1; -1 -1];
+    kernel_t1 = -1/4*ones(2,2);
+    kernel_t2 = 1/4*ones(2,2);
+    
+    Ix = convolve(Image1, kernel_x) + convolve(Image2, kernel_x);
+    Iy = convolve(Image1, kernel_y) + convolve(Image2, kernel_y);
+    It = convolve(Image1, kernel_t1) + convolve(Image2, kernel_t2);
+end
